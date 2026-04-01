211295db0d0ed42239ea407c1bea1b0141324d72
Ziad Elnagar <57187405+ZiadNagar@users.noreply.github.com>
2026-04-01 21:17:08 +0200

    update comments for more clarification

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
@@ -6,8 +7,10 @@ import App from "./App";
  * HelmetProvider must wrap the entire app so that
  * react-helmet-async can manage <head> updates across routes.
  */
-createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
 );
